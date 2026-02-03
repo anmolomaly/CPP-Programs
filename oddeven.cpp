af5a00d685853a4f3bf81aab76679d7312b732a1
ANMOL <anmol.k1420@gmail.com>
2026-02-03 09:45:00 +0530

    2 programs

---
+++ oddeven.cpp
@@ -0,0 +1,14 @@
+//WAP to check if a number is even or odd
+#include <iostream>
+ 
+ int main() {
+    int a;
+    std::cout << "Enter a number: ";
+    std::cin >> a;
+    if (a % 2 == 0) {
+        std::cout << "The number is even.";
+    } else {
+        std::cout << "The number is odd.";
+    }
+    return 0;
+ }
