fe818b2130365d0ad69329d285d1f749f4ecf382
ANMOL <anmol.k1420@gmail.com>
2026-03-23 15:21:32 +0530

    fixed ts

--- sum_of_diagonals.cpp
+++ sum_of_diagonals.cpp
@@ -10,14 +10,22 @@ int main()
     int s1=0,s2=0;
     for(int i=0;i<n;i++)
     {
-        for(int j=0;i<n;j++)
+        for(int j=0;j<n;j++)
         {
             cin>>arr[i][j];
         }
     }
     for(int i=0;i<n;i++)
     {
-        for(int j=0;i<n;j++)
+        for(int j=0;j<n;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
         {
             if(i==j)
             s1+=arr[i][j];
